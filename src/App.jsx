bb5aa4e8c6beeb78d372bac786a400b67ab9fa52
x_yonder <84139856+matheusyan-coder@users.noreply.github.com>
2023-03-20 11:38:44 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Titulo from "./Titulo"
+
+function App() { 
+return (
+  <div>
+  <Titulo/>
+  <Titulo/>
+</div>
+)
+   
+}
+
+export default App;
---
+++ src/Titulo.jsx
@@ -0,0 +1,11 @@
+function Titulo(){
+    return (
+    <div>
+    <h1>Aqui é um titulo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, voluptatum exercitationem cum totam ut culpa cumque aperiam corrupti voluptas ipsum natus beatae sint, sunt, aliquid a. Aliquam obcaecati repellendus quae!</p>
+    </div>
+    )
+}
+
+export default Titulo;
+
